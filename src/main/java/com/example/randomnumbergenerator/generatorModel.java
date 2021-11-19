63af9cc2f7781cd284563c5aba0d9ec162f70709
Mati <mateuszlabun9@gmail.com>
2021-11-19 21:10:19 +0100

    comments adjusted2

--- src/main/java/com/example/randomnumbergenerator/generatorModel.java
+++ src/main/java/com/example/randomnumbergenerator/generatorModel.java
@@ -1,6 +1,7 @@
 package com.example.randomnumbergenerator;
 
 import org.apache.commons.math3.distribution.EnumeratedIntegerDistribution;
+
 import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
@@ -34,49 +35,42 @@ public class generatorModel {
      * possibilities inserted by user. After that, generated numbers are used to create enumerated distribution,
      * with probabilities passed by user. At the end, generated values are inserted into map in order to sort them out.
      */
-    public void generateRandomNumbers(){
+    public void generateRandomNumbers() {
         int[] numbersToGenerate = new int[possibilitiesOfNumber.length];
         double[] discreteProbabilities = new double[possibilitiesOfNumber.length];
         int beginning = this.intervalOfNumbers[0];//beginning od interval
         int higherInterval = this.intervalOfNumbers[1];//ending of interval
         //span between beginning and ending of interval, divided by number of probabilities
-        int valueOfIntervals = (higherInterval -this.intervalOfNumbers[0])/ this.possibilitiesOfNumber.length;
+        int valueOfIntervals = (higherInterval - this.intervalOfNumbers[0]) / this.possibilitiesOfNumber.length;
 
         //generate random numbers, amount is equal to number of probabilities
-        for(int i =0; i<this.possibilitiesOfNumber.length;i++){
-            if((i + 1) != this.possibilitiesOfNumber.length){
+        for (int i = 0; i < this.possibilitiesOfNumber.length; i++) {
+            if ((i + 1) != this.possibilitiesOfNumber.length) {
                 numbersToGenerate[i] = ThreadLocalRandom.current().nextInt(beginning, beginning + valueOfIntervals);
-                discreteProbabilities[i]=possibilitiesOfNumber[i]*0.01;//adjusting probabilities
-                System.out.println("przedzial od: " + beginning + "do " + (beginning+valueOfIntervals));
-                beginning+=valueOfIntervals;
-            }
-            else {
-                numbersToGenerate[i] = ThreadLocalRandom.current().nextInt(beginning,higherInterval );
-                discreteProbabilities[i]=possibilitiesOfNumber[i]*0.01;//adjusting probabilities
+                discreteProbabilities[i] = possibilitiesOfNumber[i] * 0.01;//adjusting probabilities
+                System.out.println("przedzial od: " + beginning + "do " + (beginning + valueOfIntervals));
+                beginning += valueOfIntervals;
+            } else {
+                numbersToGenerate[i] = ThreadLocalRandom.current().nextInt(beginning, higherInterval);
+                discreteProbabilities[i] = possibilitiesOfNumber[i] * 0.01;//adjusting probabilities
                 System.out.println("koncowy przedzial od: " + beginning + "do " + higherInterval);
             }
         }
         System.out.println("leca wylosowane liczby" + Arrays.toString(numbersToGenerate));
         //creating enumerated distribution
         EnumeratedIntegerDistribution distribution = new EnumeratedIntegerDistribution(numbersToGenerate, discreteProbabilities);
-        //
         int numSamples = 1000000;
-        samplesArray = distribution.sample(numSamples); // losowanie 100 razy z podanych wartości
+        samplesArray = distribution.sample(numSamples);
         String[] strArray = Arrays.stream(samplesArray)
                 .mapToObj(String::valueOf)
-                .toArray(String[]::new); // konwersja na stringa
+                .toArray(String[]::new); // String conversion
 
         /*Create a Stream<String> from the original array
         Group each element by identity, resulting in a Map<String, List<String>>
         For each key value pair, add it to treemap*/
         Arrays.stream(strArray)
                 .collect(Collectors.groupingBy(s -> s))
-                .forEach((k, v) -> distributionValuesMap.put(Integer.parseInt(k),String.valueOf(v.size())));// licze wystąpienia
-
-        //Deleting created objects
-        distribution=null;
-        numbersToGenerate=null;
-        discreteProbabilities=null;
+                .forEach((k, v) -> distributionValuesMap.put(Integer.parseInt(k), String.valueOf(v.size())));
     }
 
     /**
@@ -112,7 +106,7 @@ public class generatorModel {
      * @param i the
      * @return the int
      */
-    public int getStrictIntervalOfNumbers(int i){
+    public int getStrictIntervalOfNumbers(int i) {
         return intervalOfNumbers[i];
     }
 
@@ -140,7 +134,7 @@ public class generatorModel {
      * @param i the
      * @return the int
      */
-    public int getStrictPossibilitiesOfNumber(int i ){
+    public int getStrictPossibilitiesOfNumber(int i) {
         return possibilitiesOfNumber[i];
     }
 }
